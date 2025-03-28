77b70544467714835901002e089a5ba10baa712d
Obydullah001 <hasibobydullah001@gmail.com>
2025-03-28 16:09:47 +0600

    added js

---
+++ script/complete.js
@@ -0,0 +1,85 @@
+document.getElementById('btn-complete-1').addEventListener('click', function (event) {
+    event.preventDefault()
+    event.target.setAttribute('disabled', true );
+    const accumulatedAmount = document.getElementById('accumulated-amount').innerText ;
+    const fullAccumulatedAmount = parseInt(accumulatedAmount);
+    const taskDecrease = document.getElementById('task-decrease').innerText;
+    const fullTaskDecrease = parseInt(taskDecrease);
+    const sub = fullTaskDecrease - 1 ;
+    const sum = fullAccumulatedAmount+1 ;
+    document.getElementById('task-decrease').innerText= sub;
+    document.getElementById('accumulated-amount').innerText = sum ;
+    alert('Board Updated Successfully');
+    
+    
+})
+document.getElementById('btn-complete-2').addEventListener('click', function (event) {
+    event.preventDefault()
+    event.target.setAttribute('disabled', true );
+    const accumulatedAmount = document.getElementById('accumulated-amount').innerText ;
+    const fullAccumulatedAmount = parseInt(accumulatedAmount);
+    const taskDecrease = document.getElementById('task-decrease').innerText;
+    const fullTaskDecrease = parseInt(taskDecrease);
+    const sub = fullTaskDecrease - 1 ;
+    const sum = fullAccumulatedAmount+1 ;
+    document.getElementById('task-decrease').innerText= sub;
+    document.getElementById('accumulated-amount').innerText = sum ;
+    alert('Board Updated Successfully');
+    
+})
+document.getElementById('btn-complete-3').addEventListener('click', function (event) {
+    event.preventDefault()
+    event.target.setAttribute('disabled', true );
+    const accumulatedAmount = document.getElementById('accumulated-amount').innerText ;
+    const fullAccumulatedAmount = parseInt(accumulatedAmount);
+    const taskDecrease = document.getElementById('task-decrease').innerText;
+    const fullTaskDecrease = parseInt(taskDecrease);
+    const sub = fullTaskDecrease - 1 ;
+    const sum = fullAccumulatedAmount+1 ;
+    document.getElementById('task-decrease').innerText= sub;
+    document.getElementById('accumulated-amount').innerText = sum ;
+    alert('Board Updated Successfully');
+    
+})
+document.getElementById('btn-complete-4').addEventListener('click', function (event) {
+    event.preventDefault()
+    event.target.setAttribute('disabled', true );
+    const accumulatedAmount = document.getElementById('accumulated-amount').innerText ;
+    const fullAccumulatedAmount = parseInt(accumulatedAmount);
+    const taskDecrease = document.getElementById('task-decrease').innerText;
+    const fullTaskDecrease = parseInt(taskDecrease);
+    const sub = fullTaskDecrease - 1 ;
+    const sum = fullAccumulatedAmount+1 ;
+    document.getElementById('task-decrease').innerText= sub;
+    document.getElementById('accumulated-amount').innerText = sum ;
+    alert('Board Updated Successfully');
+    
+})
+document.getElementById('btn-complete-5').addEventListener('click', function (event) {
+    event.preventDefault()
+    event.target.setAttribute('disabled', true );
+    const accumulatedAmount = document.getElementById('accumulated-amount').innerText ;
+    const fullAccumulatedAmount = parseInt(accumulatedAmount);
+    const taskDecrease = document.getElementById('task-decrease').innerText;
+    const fullTaskDecrease = parseInt(taskDecrease);
+    const sub = fullTaskDecrease - 1 ;
+    const sum = fullAccumulatedAmount+1 ;
+    document.getElementById('task-decrease').innerText= sub;
+    document.getElementById('accumulated-amount').innerText = sum ;
+    alert('Board Updated Successfully');
+    
+})
+document.getElementById('btn-complete-6').addEventListener('click', function (event) {
+    event.preventDefault()
+    event.target.setAttribute('disabled', true );
+    const accumulatedAmount = document.getElementById('accumulated-amount').innerText ;
+    const fullAccumulatedAmount = parseInt(accumulatedAmount);
+    const taskDecrease = document.getElementById('task-decrease').innerText;
+    const fullTaskDecrease = parseInt(taskDecrease);
+    const sub = fullTaskDecrease - 1 ;
+    const sum = fullAccumulatedAmount+1 ;
+    document.getElementById('task-decrease').innerText= sub;
+    document.getElementById('accumulated-amount').innerText = sum ;
+    alert('Board Updated Successfully');
+    
+})
---
+++ script/login.js
@@ -0,0 +1,5 @@
+document.getElementById('login-btn').addEventListener('click', function (event) {
+    event.preventDefault()
+    window.location.href="main.html"
+    
+})
